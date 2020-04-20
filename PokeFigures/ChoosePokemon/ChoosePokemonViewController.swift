90f505b6fd05df3f0fb37ba42101df095d950ab9
Konstantinos Reppas <k.reppas92@gmail.com>
2020-04-21 02:55:51 +0300

    initial commit - laying out the basic idea

---
+++ PokeFigures/ChoosePokemon/ChoosePokemonViewController.swift
@@ -0,0 +1,116 @@
+//
+//  ViewController.swift
+//  PokeFigures
+//
+//  Created by Konstantinos Reppas on 20/4/20.
+//  Copyright © 2020 Konstantinos Reppas. All rights reserved.
+//
+
+import UIKit
+
+class ChoosePokemonViewController: UIViewController {
+    
+    @IBOutlet weak var pokemonCollectionView: UICollectionView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        pokemonCollectionView.showsHorizontalScrollIndicator = false
+        let padding = pokemonCollectionView.bounds.size.width * CGFloat(((1 - C.POKEMON_CELL_WIDTH_PERCENTAGE) * 0.5))
+        pokemonCollectionView.contentInset = UIEdgeInsets(top: 0, left: padding, bottom: 0, right: padding)
+        
+        
+        let pokemonCellNib = UINib(nibName: "ChoosePokemonCollectionViewCell", bundle: nil)
+        pokemonCollectionView.register(pokemonCellNib, forCellWithReuseIdentifier: "ChoosePokemonCollectionViewCell")
+        
+        pokemonCollectionView.dataSource = self
+        pokemonCollectionView.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        pokemonCollectionView.reloadData()
+        
+        Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(resizeCells), userInfo: nil, repeats: false)
+    }
+}
+
+extension ChoosePokemonViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ChoosePokemonCollectionViewCell", for: indexPath)
+        
+        return cell
+    }
+    
+}
+
+extension ChoosePokemonViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.bounds.size.width * CGFloat(C.POKEMON_CELL_WIDTH_PERCENTAGE), height: collectionView.bounds.size.height * 0.95)
+    }
+}
+
+extension ChoosePokemonViewController : UIScrollViewDelegate {
+    // perform scaling whenever the collection view is being scrolled
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+        resizeCells()
+    }
+    
+    // for custom snap-to paging, when user stop scrolling
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        
+        var indexOfCellWithLargestWidth = 0
+        var largestWidth : CGFloat = 1
+        
+        for cell in pokemonCollectionView.visibleCells {
+            if cell.frame.size.width > largestWidth {
+                largestWidth = cell.frame.size.width
+                if let indexPath = pokemonCollectionView.indexPath(for: cell) {
+                    indexOfCellWithLargestWidth = indexPath.item
+                }
+            }
+        }
+        
+        pokemonCollectionView.scrollToItem(at: IndexPath(item: indexOfCellWithLargestWidth, section: 0), at: .centeredHorizontally, animated: true)
+    }
+    
+    @objc func resizeCells() {
+        // center X of collection View
+        let centerX = self.pokemonCollectionView.center.x
+        
+        // only perform the scaling on cells that are visible on screen
+        for cell in self.pokemonCollectionView.visibleCells {
+            
+            
+            
+            // coordinate of the cell in the viewcontroller's root view coordinate space
+            let basePosition = cell.convert(CGPoint.zero, to: self.view)
+            let cellCenterX = basePosition.x + self.pokemonCollectionView.frame.size.width / 2
+            
+            let distance = abs(cellCenterX - centerX)
+            
+            let tolerance : CGFloat = 0.02
+            var scaleY = 1.00 + tolerance - (( distance / centerX) * 0.09)
+            if(scaleY > 1.0){
+                scaleY = 1.0
+            }
+            
+            var scaleX = 1.00 + tolerance - (( distance / centerX) * 0.035)
+            if(scaleX > 1.0){
+                scaleX = 1.0
+            }
+            
+            print( String(Float(distance)))
+            
+            // Transform the cell size based on the scale
+            cell.transform = CGAffineTransform(scaleX: scaleX, y: scaleY)
+        }
+    }
+}
---
+++ PokeFigures/Constants/C.swift
@@ -0,0 +1,13 @@
+//
+//  C.swift
+//  PokeFigures
+//
+//  Created by Konstantinos Reppas on 20/4/20.
+//  Copyright © 2020 Konstantinos Reppas. All rights reserved.
+//
+
+import Foundation
+
+struct C {
+    public static let POKEMON_CELL_WIDTH_PERCENTAGE = 0.85
+}
